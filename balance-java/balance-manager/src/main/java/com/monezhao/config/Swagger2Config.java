761943de3917fa3bd40222d556c43e2753d9303f
Mone Zhao <monezhao@163.com>
2020-07-14 14:25:41 +0800

    swagger2设置请求token

--- balance-java/balance-manager/src/main/java/com/monezhao/config/Swagger2Config.java
+++ balance-java/balance-manager/src/main/java/com/monezhao/config/Swagger2Config.java
@@ -13,13 +13,18 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.Parameter;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -55,7 +60,32 @@ public class Swagger2Config implements WebMvcConfigurer {
                 .apis(RequestHandlerSelectors.basePackage("com.monezhao.controller"))
                 // 加了ApiOperation注解的类，才生成接口文档
                 .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)).paths(PathSelectors.any()).build()
-                .globalOperationParameters(setHeaderToken());
+                .securitySchemes(securitySchemes())
+                .securityContexts(securityContexts())
+//                .globalOperationParameters(setHeaderToken())
+                ;
+    }
+
+    private List<ApiKey> securitySchemes() {
+        return Arrays.asList(
+                new ApiKey("token", "token", "header"));
+    }
+
+    private List<SecurityContext> securityContexts() {
+        return Arrays.asList(
+                SecurityContext.builder()
+                        .securityReferences(defaultAuth())
+                        .forPaths(PathSelectors.any())
+                        .build()
+        );
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(
+                new SecurityReference("token", authorizationScopes));
     }
 
     private List<Parameter> setHeaderToken() {
