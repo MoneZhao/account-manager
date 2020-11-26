7222d2d0c9ab94807707fc9401ad63e53fdc61fe
Mone Zhao <monezhao@163.com>
2020-11-26 20:03:56 +0800

    过滤为空的sql

--- balance-java/balance-system/src/main/java/com/monezhao/config/CustomSqlFormat.java
+++ balance-java/balance-system/src/main/java/com/monezhao/config/CustomSqlFormat.java
@@ -1,5 +1,6 @@
 package com.monezhao.config;
 
+import com.monezhao.common.util.CommonUtil;
 import com.p6spy.engine.spy.appender.MessageFormattingStrategy;
 
 /**
@@ -10,6 +11,10 @@ public class CustomSqlFormat implements MessageFormattingStrategy {
     @Override
     public String formatMessage(int connectionId, String now, long elapsed, String category,
                                 String prepared, String sql, String url) {
-        return "\n# " + now + " | 执行sql用时 " + elapsed + "ms" + "\n==>    SQl开始:\n" + sql + "\n<==    SQL结束";
+        if (CommonUtil.isEmptyStr(sql)) {
+            return "";
+        } else {
+            return "\n# " + now + " | 执行sql用时 " + elapsed + "ms" + "\n==>    SQl开始:\n" + sql + "\n<==    SQL结束";
+        }
     }
 }
