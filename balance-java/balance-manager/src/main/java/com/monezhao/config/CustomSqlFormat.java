5288c60b0134f596bb5f46b204ecec2c8cc8157d
Mone Zhao <monezhao@163.com>
2022-06-29 08:07:40 +0800

    sql cost over 200ms, log

--- balance-java/balance-manager/src/main/java/com/monezhao/config/CustomSqlFormat.java
+++ balance-java/balance-manager/src/main/java/com/monezhao/config/CustomSqlFormat.java
@@ -2,11 +2,13 @@ package com.monezhao.config;
 
 import com.monezhao.common.util.CommonUtil;
 import com.p6spy.engine.spy.appender.MessageFormattingStrategy;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author monezhao@163.com
  * @date 2020/11/26 7:35 下午
  */
+@Slf4j
 public class CustomSqlFormat implements MessageFormattingStrategy {
     @Override
     public String formatMessage(int connectionId, String now, long elapsed, String category,
@@ -14,6 +16,9 @@ public class CustomSqlFormat implements MessageFormattingStrategy {
         if (CommonUtil.isEmptyStr(sql)) {
             return "";
         } else {
+            if (elapsed > 200) {
+                log.info("用时超过200ms!!!");
+            }
             return "\n# " + now + " | 执行sql用时 " + elapsed + "ms" + "\n==>    SQl开始:\n" + sql + "\n<==    SQL结束";
         }
     }
